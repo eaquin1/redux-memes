2f842838dfe6610192d710e06739fb4f4f4da04c
Emily Aquin <eaquin1@gmail.com>
2020-07-09 17:06:02 -0500

    added tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import {createStore} from "redux"
+import {Provider} from "react-redux"
+import rootReducer from "./rootReducer"
 
+const store = createStore(rootReducer);
+
+test("renders title", () => {
+    const {getByText} = render(<Provider store={store}><App /></Provider>)
+    const titleElement = getByText(/MEME GENERATOR/i);
+    expect(titleElement).toBeInTheDocument()
+})
